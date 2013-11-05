6d43c12eef30ceb6bcc87318b0fe81a366ac0c66
kestred <kestred@riotcave.com>
2013-11-05 12:33:36 -0700

    API: Write ClientConnection API.

--- src/connection/ClientConnection.h
+++ src/connection/ClientConnection.h
@@ -4,24 +4,23 @@
 //     If necessary connection can subclass another class (maybe NetworkClient for example);
 //     However, any inherited virtual functions must be implemented. Subclasses of connection
 //     should only be required to implement handle_datagram.
-class InternalConnection
+class ClientConnection
 {
 	public:
-		InternalConnection(std::string addr = "127.0.0.1", uint16_t port = 7199);
+		ClientConnection(std::string uri);
 
-		// Control messages
-		void subscribe_channel(channel_t channel);
-		void unsubscribe_channel(channel_t channel);
-		void subscribe_range(channel_t lo, channel_t hi);
-		void unsubscribe_range(channel_t lo, channel_t hi);
-		void add_post_remove(const Datagram &dg);
-		void clear_post_removes();
-		void set_conn_url(std::string uri);
-		void set_conn_name(std::string name);
+		// connect starts a connection to the server, negotiates Hello and starts sending
+		// heartbeats periodically. It returns after negotiation is complete.
+		void connect(uint32_t dc_hash, std::string version);
+
+		// disconnect cleanly closes the connection.
+		// Internally: Sends a CLIENT_DISCONNECT, and waits for connection to close.
+		void disconnect();
 
 		// Outgoing datagram
 		void send_datagram(const Datagram &dg);
 
 	protected:
 		virtual void handle_datagram(const Datagram &dg) = 0;
+		virtual void handle_eject(std::string reason) = 0;
 };
