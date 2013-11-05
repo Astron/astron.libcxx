c011dbbf067e43650c515c0a2dc69c335d43a193
kestred <kestred@riotcave.com>
2013-11-05 13:38:47 -0700

    API: Define the API through use case by writing the skeleton of a guide for Uberdogs.

---
+++ doc/guides/CreateASimpleUberdog.md
@@ -0,0 +1,39 @@
+Creating A Simple Uberdog
+---------------------------
+
+### The Main routine ###
+//TODO: Describe the code snippets
+
+**With quick setup**
+```c_cpp
+#include <libastron-cxx/InternalRepository.h>
+#include "MyUberdog.h"
+
+using namespace astron;
+
+#define UBERDOG_ID 10002
+
+int main(int argc, const char* argv[])
+{
+	MyUberdog* ud = new MyUberdog(UBERDOG_ID);
+	InternalRepository air = InternalRepository(ud);
+}
+```
+
+**With manual setup**
+```
+int main(int argc, const char* argv[])
+{
+	MyUberdog* ud = new MyUberdog(UBERDOG_ID);
+	InternalRepository air = InternalRepository();
+	air.attach_object(ud);
+
+	// do some other fancy setup
+
+	air.connect();
+	air.subscribe_channel(UBERDOG_ID);
+}
+```
+
+### Defining the Uberdog Behavior ###
+//TODO: Document and provide examples for subclassing the DistributedObject class.
--- src/repository/InternalRepository.h
+++ src/repository/InternalRepository.h
@@ -7,12 +7,18 @@ class InternalRepository : public ObjectRepository, InternalConnection
 		// Manual constructor.
 		InternalRepository();
 
-		// Autorun constructor: Connects to the cluster and subscribes to the channel argument.
-		InternalRepository(std::string addr, uint16_t port, channel_t channel);
+		// Autorun AI constructors: Connects to the cluster and subscribes to the channel argument.
+		InternalRepository(channel_t channel);
+		InternalRepository(channel_t channel, std::string addr, uint16_t port);
+
+		// Autorun object constructor: Attaches the object argument, connects to the cluster,
+		//                             and subscribes to the object's id.
+		InternalRepository(DistributedObject* object);
+		InternalRepository(DistributedObject* object, std::string addr, uint16_t port);
 
 		// attach_object adds a locally hosted object to the repository.
 		//     Typically this is called when hosting an UberDOG.
-		void attach_object(DistributedObject object);
+		void attach_object(DistributedObject* object);
 
 		// attach_object_view subscribes to a remotely hosted object with view argument.
 		//     Typically this is used to listen to an UberDOG.
